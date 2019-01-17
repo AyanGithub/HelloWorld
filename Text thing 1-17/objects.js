6e7ce4af103e181a050c3790f94144e79d21692d
AyanGithub <AyanGithub@users.noreply.github.com>
2019-01-17 15:45:30 -0500

    Completely Revamped! Built from the GROUND UP!
    Actually, just copiedf code
    
    Co-Authored-By: ayangithub <ayangithub@users.noreply.github.com>

--- Text thing 1-17/objects.js
+++ Text thing 1-17/objects.js
@@ -1,18 +1,42 @@
 let c = document.getElementById("canvas");
 let ctx = c.getContext('2d');
 
-c.width=window.innerWidth;
-c.height=window.innerHeight;
+c.width = window.innerWidth;
+c.height = window.innerHeight;
 
-window.oneresize = () => {
-  c.width=window.innerWidth;
-  c.height=window.innerHeight;
+window.onresize = () => {
+   c.width = window.innerWidth;
+   c.height = window.innerHeight;
 };
 
-class Player{
-  constructor(x,y,w,h){
-    this.x=x;
-    this.y=y;
-    this.w=w;
-    this.h=h;
+class Player {
+   constructor(x, y, w, h) {
+       this.x = x;
+       this.y = y;
+       this.w = w;
+       this.h = h;
+   }
+
+   update(pixels) {
+       this.move(pixels);
+       this.draw();
+   }
+
+   move(pixels) {
+       this.x += pixels;
+   }
+
+   draw() {
+       ctx.fillRect(this.x, this.y, this.w, this.h);
+   }
 }
+
+let peoples = [new Player(10, 10, 100, 100), new Player(400, 400, 100, 300)];
+
+function animate() {
+   ctx.clearRect(0, 0, c.width, c.height);
+   for (let player of peoples)
+       player.update(3);
+}
+
+setInterval(animate, 10);
