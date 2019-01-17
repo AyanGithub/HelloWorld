7c1a352ee1c1cff4ffeff2e0309c29f58f31624a
AyanGithub <AyanGithub@users.noreply.github.com>
2019-01-17 15:00:36 -0500

    changes

--- Text thing 1-17/timeout.js
+++ Text thing 1-17/timeout.js
@@ -1,11 +1,15 @@
 function printRandom() {
    console.log(Math.floor(Math.random() * 10) + 1);
+   console.log("HI");
 }
+let x = 0;
+setTimeout(printRandom, 400);
+
 
 setTimeout(printRandom, 7000);
 
-setInterval(() => console.log("A Second Passed!"), 1000);
+setInterval(() => console.log("A Second Passed!" + (++x)), 1000);
 
 window.onresize = () => {
-   console.log("Window Size Changed!");
+   console.log("Window Rechanged!");
 };
